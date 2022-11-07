8345c3fa0808257abc60281b97befad694ac0956
Tom Chestnut <tchestnut85@gmail.com>
2022-11-07 15:54:51 -0500

    add initial snake_game and snake class

---
+++ snake_game/main.py
@@ -0,0 +1,35 @@
+# pylint: disable=no-member
+
+from turtle import Screen
+import time
+
+from snake import Snake
+
+
+def start_game():
+    snake = Snake()
+
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
+
+    screen.listen()
+    screen.onkey(key="Up", fun=snake.up)
+    screen.onkey(key="Down", fun=snake.down)
+    screen.onkey(key="Left", fun=snake.left)
+    screen.onkey(key="Right", fun=snake.right)
+
+    game_is_on = True
+
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+
+        snake.move()
+
+    screen.exitonclick()
+
+
+start_game()
---
+++ snake_game/snake.py
@@ -0,0 +1,50 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+DIRECTIONS = {
+    "UP": 90,
+    "DOWN": 270,
+    "LEFT": 180,
+    "RIGHT": 0
+}
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            part = Turtle(shape="square")
+            part.penup()
+            part.color("white")
+            part.goto(position)
+
+            self.segments.append(part)
+
+    def move(self):
+        for segment in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[segment - 1].xcor()
+            new_y = self.segments[segment - 1].ycor()
+            self.segments[segment].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DIRECTIONS["DOWN"]:
+            self.head.setheading(DIRECTIONS["UP"])
+
+    def down(self):
+        if self.head.heading() != DIRECTIONS["UP"]:
+            self.head.setheading(DIRECTIONS["DOWN"])
+
+    def left(self):
+        if self.head.heading() != DIRECTIONS["RIGHT"]:
+            self.head.setheading(DIRECTIONS["LEFT"])
+
+    def right(self):
+        if self.head.heading() != DIRECTIONS["LEFT"]:
+            self.head.setheading(DIRECTIONS["RIGHT"])
